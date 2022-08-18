56e36a5b734938e9806e0ce4244281ccacb93f45
xmuyzz <xmuyzz@gmail.com>
2022-08-18 09:22:15 -0400

    dataset

---
+++ dfci_curation/TCIA_preprocess.py
@@ -0,0 +1,217 @@
+### Initiate this file after nrrds have been generated for labels and images in run_bk file 
+## Benjamin Kann
+### Order of operations: combine label nrrds, interpolate image, interpolate combined label, crop roi
+
+import sys
+import os
+import pydicom
+import glob
+import SimpleITK as sitk
+from dcm_to_nrrd import dcm_to_nrrd
+from rtstruct_to_nrrd import rtstruct_to_nrrd
+from combine_structures import combine_structures
+from interpolate import interpolate
+from crop import crop_roi, crop_top, crop_top_image_only
+from registration import nrrd_reg_rigid
+import SimpleITK as sitk
+import shutil
+
+
+
+def transfer_file(raw_data_dir, proj_dir):
+    
+    """
+    1) Transfer image, pn seg, n seg and p seg file to organzied folder;
+    2) rename files;
+    """
+
+    CHUS_dir = raw_data_dir + '/CHUS_files/interpolated'
+    CHUM_dir = raw_data_dir + '/CHUM_files/interpolated'
+    MDACC_dir = raw_data_dir + '/MDACC_files/interpolated'
+    PMH_dir = raw_data_dir + '/PMH_files/interpolated'
+    dst_img_dir = proj_dir + '/TCIA/imgs'
+    pn_seg_dir = proj_dir + '/TCIA/pn_segs'
+    p_seg_dir = proj_dir + '/TCIA/p_segs'
+    n_seg_dir = proj_dir + '/TCIA/n_segs'
+    if not os.path.exists(dst_img_dir):
+        os.makedirs(dst_img_dir)
+    if not os.path.exists(pn_seg_dir):
+        os.makedirs(pn_seg_dir)
+    if not os.path.exists(p_seg_dir):
+        os.makedirs(p_seg_dir)
+    if not os.path.exists(n_seg_dir):
+        os.makedirs(n_seg_dir)
+    
+    # dataset
+    count = 0
+    #data_dirs = [CHUM_dir, CHUS_dir, MDACC_dir, PMH_dir]
+    #cohorts = ['CHUM', 'CHUS', 'MDACC', 'PMH']
+    data_dirs = [MDACC_dir, PMH_dir]
+    cohorts = ['MDACC', 'PMH']
+    for data_dir, cohort in zip(data_dirs, cohorts):
+        for path in sorted(glob.glob(data_dir + '/*nrrd')):
+            if cohort in ['CHUM', 'CHUS']:
+                ID = path.split('/')[-1].split('_')[1].split('-')[2]
+            elif cohort == 'MDACC':
+                ID = path.split('/')[-1].split('_')[1].split('-')[2][1:]
+            elif cohort == 'PMH':
+                ID = path.split('/')[-1].split('_')[1].split('-')[1][2:]
+            fn = cohort + '_' + ID + '.nrrd'
+            data_type = path.split('/')[-1].split('_')[2]
+            count += 1
+            print(count, fn)
+            if data_type == 'ct':
+                dst_dir = dst_img_dir + '/' + fn
+                print(path)
+                print(dst_dir)
+                shutil.copyfile(path, dst_dir)
+            elif data_type == 'label':
+                label = path.split('/')[-1].split('_')[3]
+                if label == 'interpolated':
+                    dst_dir = pn_seg_dir + '/' + fn
+                elif label == 'n':
+                    dst_dir = n_seg_dir + '/' + fn
+                elif label == 'p':
+                    dst_dir = p_seg_dir + '/' + fn
+                print(path)
+                print(dst_dir) 
+                shutil.copyfile(path, dst_dir)
+
+
+def registration(proj_dir):
+    """
+    Rigid Registration - followed by top crop
+    """
+    img_interp_dir = os.path.join(proj_dir, 'DFCI/img_interp')
+    seg_interp_dir = os.path.join(proj_dir, 'DFCI/seg_interp')
+    img_reg_dir = os.path.join(proj_dir, 'DFCI/img_reg')
+    seg_reg_dir = os.path.join(proj_dir, 'DFCI/seg_reg')
+    fixed_img_dir = os.path.join(proj_dir, 'DFCI/img_interp/10020741814.nrrd')
+    if not os.path.exists(img_reg_dir):
+        os.makedirs(img_reg_dir)
+    if not os.path.exists(seg_reg_dir):
+        os.makedirs(seg_reg_dir)
+    # register images
+    count = 0
+    img_ids = []
+    img_dirs = [i for i in sorted(glob.glob(img_interp_dir + '/*nrrd'))]
+    seg_dirs = [i for i in sorted(glob.glob(seg_interp_dir + '/*nrrd'))]
+    for img_dir in img_dirs:
+        img_id = img_dir.split('/')[-1].split('.')[0]
+        for seg_dir in seg_dirs:
+            seg_id = seg_dir.split('/')[-1].split('.')[0]
+            if img_id == seg_id:
+                img_ids.append(img_id)
+                count += 1
+                print(count)
+                print(img_id)
+                try:
+                    # register images
+                    fixed_image, moving_image, final_transform = nrrd_reg_rigid( 
+                        patient_id=img_id, 
+                        input_dir=img_dir, 
+                        output_dir=img_reg_dir, 
+                        fixed_img_dir=fixed_img_dir)
+                    # register segmentations
+                    moving_label = sitk.ReadImage(seg_dir, sitk.sitkFloat32)
+                    moving_label_resampled = sitk.Resample(
+                        moving_label, 
+                        fixed_image, 
+                        final_transform, 
+                        sitk.sitkNearestNeighbor, 
+                        0.0, 
+                        moving_image.GetPixelID())
+                    sitk.WriteImage(
+                        moving_label_resampled, 
+                        os.path.join(seg_reg_dir, img_id + '.nrrd'))
+                    #transform = sitk.ReadTransform('.tfm')
+                except Exception as e:
+                    print(e, 'segmentation registration failed')
+    for img_dir in img_dirs:
+        img_id = img_dir.split('/')[-1].split('.')[0]
+        if img_id not in img_ids:
+            count += 1
+            print(count)
+            print(img_id)
+            try:
+                fixed_image, moving_image, final_transform = nrrd_reg_rigid(
+                    patient_id=img_id,
+                    input_dir=img_dir,
+                    output_dir=img_reg_dir,
+                    fixed_img_dir=fixed_img_dir)
+            except Exception as e:
+                print(e)
+
+
+def crop(proj_dir):
+    
+    """
+    With TOP-CROP HPC ### NEED TO RUN FOR image_crop, image_crop_p, and image_crop_n  
+    WILL ONLY WORK WITH SPACING = 1,1,3
+    """
+
+    roi_size = (172, 172, 76) #x,y,z
+    size_str = '172 x 172 x 76'
+    img_dirs = [i for i in sorted(glob.glob(dfci_img_dir + '/*nrrd'))]
+    seg_dirs = [i for i in sorted(glob.glob(dfci_seg_dir + '/*nrrd'))]
+    img_crop_dir = proj_dir + '/DFCI/img_crop'
+    seg_crop_dir = proj_dir + '/DFCI/seg_crop'
+    if not os.path.exists(img_crop_dir):
+        os.makedirs(img_crop_dir)
+    if not os.path.exists(seg_crop_dir):
+        os.makedirs(seg_crop_dir)
+    img_dirs = [i for i in sorted(glob.glob(img_reg_dir + '/*nrrd'))]
+    seg_dirs = [i for i in sorted(glob.glob(seg_reg_dir + '/*nrrd'))]
+    for img_dir in img_dirs:
+        img_id = img_dir.split('/')[-1].split('.')[0]
+        for seg_dir in seg_dirs:
+            seg_id = seg_dir.split('/')[-1].split('.')[0]
+            if img_id == seg_id:
+                img_ids.append(img_id)
+                try:
+                    image_obj, label_obj = crop_top(
+                        patient_id=img_id,
+                        img_dir=img_dir,
+                        seg_dir=seg_dir,
+                        crop_shape=roi_size,
+                        return_type='sitk_object',
+                        output_img_folder=output_img_folder,
+                        output_seg_folder=output_seg_folder)
+                except:
+                    print('crop failed!')
+    for img_dir in img_dirs:
+        img_id = img_dir.split('/')[-1].split('.')[0]
+        if img_id not in img_ids:
+            count += 1
+            print(count)
+            print(img_id)
+            try:
+                image_obj, label_obj = crop_top_image_only(
+                    patient_id,
+                    img_dir,
+                    crop_shape=roi_size,
+                    return_type='sitk_object',
+                    output_img_folder=output_img_folder)
+            except Exception as e:
+                print(e)                        
+
+
+
+if __name__ == '__main__':
+
+    proj_dir = '/mnt/aertslab/USERS/Zezhong/HN_OUTCOME'
+    raw_data_dir = '/mnt/aertslab/DATA/HeadNeck/HN_PETSEG/curated' 
+
+    do_transfer_file = True
+    do_register = False
+    do_crop = False
+
+    if do_transfer_file:
+        transfer_file(raw_data_dir, proj_dir)
+    if do_interpolate:
+        interpolate_img(proj_dir)    
+    if do_register:
+        registration(proj_dir)
+
+
+    
